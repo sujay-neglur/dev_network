905c8b4d286ec43d52f7396cca2c05b43de0c2ee
sujayneglur123@gmail.com <sujayneglur123@gmail.com>
2019-09-09 01:19:05 -0400

    Deployment

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,13 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
-const keys = require("./keys");
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
+
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
+
 module.exports = passport => {
     passport.use(
         new JwtStrategy(opts, (jwt_payload, done) => {
@@ -16,9 +18,7 @@ module.exports = passport => {
                     }
                     return done(null, false);
                 })
-                .catch(err => {
-                    console.log(err);
-                });
+                .catch(err => console.log(err));
         })
     );
 };
